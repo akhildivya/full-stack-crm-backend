ed4fed13f11f97239e191216bae60ff9c0d655b5
DIVYA <divyaks799@gmail.com>
2025-08-21 09:10:45 +0530

    Third commit

--- controllers/userLogic.js
+++ controllers/userLogic.js
@@ -17,18 +17,18 @@ const userRegister = async (req, res) => {
         }
         const existingUser = await User.findOne({ email })
         if (existingUser) {
-            res.status(400).json("User already exists")
+            res.status(409).json("User already exists!")
         }
         else {
             const hashPad = await bcrypt.hash(password, 10)
             const newUser = new User({ username, email, password: hashPad })
             await newUser.save()
-            res.status(200).json(`registered with username ${username}`)
+            res.status(200).json(`Registered with username ${username}`)
         }
 
     }
     catch (err) {
-        res.status(401).json({ message: "User register API not working" })
+        res.status(500).json({ message: "User register API not working" })
     }
 }
 
@@ -43,11 +43,11 @@ const userLogin = async (req, res) => {
             res.status(200).json({ user: currentUser, token })
         }
         else {
-            res.status(404).json("incorrect username or password")
+            res.status(401).json("Invalid username or password")
         }
     }
     catch (err) {
-        res.status(401).json("Login Api not working")
+        res.status(500).json("Login Api not working")
     }
 }
 const testController = async (req, res) => {
@@ -61,9 +61,10 @@ const forgotPasswordController = async (req, res) => {
         //  console.log(user);
 
         if (!user) {
-            res.status(401).json({ message: "user not exists" })
+            res.status(404).json({ message: "The user does not exist" })
         }
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' })
+        const secret = process.env.JWT_SECRET + user.password;
+        const token = jwt.sign({ id: user._id },  secret, { expiresIn: '1h' })
         const resetURL = `${process.env.CLIENT_URL}/reset-password/${user._id}/${token}`
         const transporter = nodemailer.createTransport({
             service: 'gmail',
@@ -79,7 +80,7 @@ const forgotPasswordController = async (req, res) => {
             text: `Click here to reset your password: ${resetURL}`
         }
         await transporter.sendMail(mailOptions)
-        res.status(200).json({ message: "Password Reset request send to your email id" })
+        res.status(200).json({ message: `Password reset request send to ${user.email}` })
 
     }
     catch (err) {
@@ -92,18 +93,19 @@ const resetPasswordController = async (req, res) => {
     const { password } = req.body
     const user = await User.findById(id)
     if (!user) {
-        res.status(400).json({ message: 'Invalid or expired' })
+        return res.status(400).json({ message: 'Invalid or expired' })
     }
+    const secret = process.env.JWT_SECRET + user.password
     try {
-        jwt.verify(token)
+        jwt.verify(token, secret)
     }
     catch (err) {
-        res.status(400).json({ message: "Invalid" })
+        return res.status(400).json({ message: "Invalid or expired token" })
     }
     const hashedPassword = await bcrypt.hash(password, 10)
     user.password = hashedPassword
     await user.save()
-    res.status(200).json({ message: 'Password has been reset' })
+    return res.status(200).json({ message: 'Password has been reset' })
 }
 
 module.exports = { userRegister, userLogin, testController, forgotPasswordController, resetPasswordController }
